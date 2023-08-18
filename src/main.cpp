a4c40489cedc3efa06206d029f902e1874ada37c
Aaron Danen <aaron.danen@gmail.com>
2023-08-17 20:35:22 -0700

    Added comments to main.cppw

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,16 @@
 int main()
 {
     Gamebox box = Gamebox();
+
+    // Allocate all the games on the heap and add them to the game box
+    // ??? Shouldn't I only allocate the game once the user picks it ???
     Game* h = new Hangman();
     box.add_game(h);
+
+    // User picks and plays a game
     box.start();
+
+    // Don't cause a memory leak
     delete h;
     return 0;
 }
